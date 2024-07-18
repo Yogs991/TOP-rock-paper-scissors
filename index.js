8114b30f2853d112a817f5d962a35a7845c705d0
Yog99 <124916155+Yogs991@users.noreply.github.com>
2024-07-18 22:58:14 +0300

    Edited js file for the 5 rounds

--- index.js
+++ index.js
@@ -7,29 +7,74 @@ rockButton.addEventListener('click',()=>playRound('rock'));
 paperButton.addEventListener('click',()=>playRound('paper'));
 scissorsButton.addEventListener('click',()=>playRound('scissors'));
 
-function playRound(playerChoice){
-    const choices = ["rock","paper","scissors"];
-    const computerChoice = choices[Math.floor(Math.random()*choices.length)];
-
-    if(playerChoice===computerChoice){
-        resultDisplay.textContent = "it's a draw";
-    }else if(
-        (playerChoice==="rock" && computerChoice==="scissors") ||
-        (playerChoice==="paper" && computerChoice==="rock") ||
-        (playerChoice==="scissors" && computerChoice==="paper")
-    ){
-        resultDisplay.textContent = "You win!";
-        playerScore++;
-    }else{
-        resultDisplay.textContent = "Computer lose!";
-        computerScore++;
-    }
-
-    playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
-    computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
-}
-
 let playerScore = 0;
 let computerScore = 0;
+let currentRound = 0;
+let totalRounds = 5;
 const playerScoreDisplay = document.querySelector("#player-score");
 const computerScoreDisplay = document.querySelector("#computer-score");
+
+const roundDisplay = document.querySelector("#round");
+
+function playRound(playerChoice){
+    if(currentRound <= totalRounds){
+        roundDisplay.textContent = `Round: ${currentRound} of ${totalRounds}`;
+        currentRound++;
+        const choices = ["rock","paper","scissors"];
+        const computerChoice = choices[Math.floor(Math.random()*choices.length)];
+
+        if(playerChoice===computerChoice){
+            resultDisplay.textContent = "it's a draw";
+        }else if(
+            (playerChoice==="rock" && computerChoice==="scissors") ||
+            (playerChoice==="paper" && computerChoice==="rock") ||
+            (playerChoice==="scissors" && computerChoice==="paper")
+        ){
+            resultDisplay.textContent = "You win!";
+            playerScore++;
+        }else{
+            resultDisplay.textContent = "Computer lose!";
+            computerScore++;
+        }
+
+        playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
+        computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+    }
+    if(currentRound>totalRounds){
+        endGame();
+    }
+}
+
+function endGame(){
+    const container = document.querySelector('#rps');
+    const choices = document.querySelector("#choice");
+    const gameInfo = document.querySelector("#game-info");
+    const roundRes = document.querySelector("#result");
+    if(choices){
+        choices.style.display = "none";
+    }
+    if(gameInfo){
+        gameInfo.style.display = "none";
+    }
+    if(roundRes){
+        roundRes.style.display = "none";
+    }
+
+    const gameConclusion = document.createElement("div");
+    gameConclusion.setAttribute('id', 'game-conclusion');
+
+    let finalMessage = '';
+    if(playerScore>computerScore){
+        finalMessage = `You win! ${playerScore} to ${computerScore}`
+    }else if(computerScore>playerScore){
+        finalMessage = `You lose! ${playerScore} to ${computerScore}`
+    }else{
+        finalMessage = `It's a draw! ${playerScore} to ${computerScore}`
+    }
+
+    gameConclusion.innerHTML = `
+    <h2>Game Over</h2>
+    <p>${finalMessage}</p>
+    <p>Score - You: ${playerScore} - Computer: ${computerScore}</p>
+}
+
